06d406e6eec3df47cc163d24ccb9cff3d4b25680
orozcoding <orozcoding@gmail.com>
2022-07-24 17:39:42 -0400

    Style book a session button

--- src/sections/Book.js
+++ src/sections/Book.js
@@ -1,5 +1,3 @@
-import Calendly from "../components/Calendly";
-
 const BookSection = () => {
   return (
     <section className="sct-book d-flex col">
@@ -8,10 +6,16 @@ const BookSection = () => {
         My current rate is $12 / session.
         Each session is 50 min long.
       </p>
-      <Calendly />
+      <a target="_blank" rel="noreferrer"
+        href="https://calendly.com/angelspanishcom/session"
+        className="book-btn d-flex">
+        <p>Book a session!</p>
+      </a>
       <p className="bk-disclaimer">No advance payment is required. Pay after our first session!</p>
       <p className="title"> Some Reviews </p>
-      <div data-romw-token="xjlAeJMCE9K7Bhp3dhvoGWH5Bpst9BkjUclVengHgPKUQmvB6h"></div>
+      <div className="bk-reviews-ctn">
+        <div data-romw-token="xjlAeJMCE9K7Bhp3dhvoGWH5Bpst9BkjUclVengHgPKUQmvB6h"></div>
+      </div>
     </section>
   )
 }
