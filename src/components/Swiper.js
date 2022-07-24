ccdaa0997a4d0613210c7d108465d04ca41d7013
orozcoding <orozcoding@gmail.com>
2022-07-24 16:25:22 -0400

    Set better Swiper

---
+++ src/components/Swiper.js
@@ -0,0 +1,45 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
+const Carousel = () => {
+  const imagesAmount = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  const renderSlide = (number) => {
+    return (
+      <SwiperSlide className="swiper-slice"
+      key={number}>
+        <img
+          className="carousel-pic"
+          src={`./img/ss/ss${number}.jpg`}
+          alt={`carousel-img-${number}`} />
+      </SwiperSlide>
+    )
+  }
+
+  return (
+    <div className="swiper-ctn">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        autoplay
+        pagination={{ clickable: true }}
+      >
+        {imagesAmount.map((number) => {
+          return renderSlide(number)
+        })}
+      </Swiper>
+
+    </div>
+  );
+};
+
+export default Carousel;
--- src/sections/Sessions.js
+++ src/sections/Sessions.js
@@ -1,11 +1,13 @@
+import Carousel from "../components/Swiper";
+
 const SessionsSection = () => {
   return (
-    <section className="sct-sessions d-flex col">
+    <section className="sct-sessions d-flex col oh">
       <h2 className="ses-title title">Our Sessions</h2>
       <p>
         I believe everyone is different so I offer 1-on-1 video meetings. This way I'm able to customize the sessions and the experience for each student. Our sessions will be unique, fulfilling your needs and finding the best path for you.
       </p>
-      <div>CARRUSEL</div>
+      <div className="ses-carousel-ctn d-flex"><Carousel /></div>
       <p>
         I have experience teaching total beginners as well as advanced students who just want to sharpen their Spanish skills. From guys wanting me to help them with Spanish pick-up lines, to doctors wanting to improve their communication with their patients, to people hiring me only to help them pass Spanish-related college exams. Doesn't matter how much Spanish you know (or if you literally just don't know any), I'm capable of help you achieve your goals.
       </p>
