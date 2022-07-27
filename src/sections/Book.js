bf1da0040ae4a635f0e38a8171ccf8dc3ee56c01
orozcoding <orozcoding@gmail.com>
2022-07-26 21:27:17 -0400

    Update style for Book sect

--- src/sections/Book.js
+++ src/sections/Book.js
@@ -1,24 +1,33 @@
 const BookSection = (props) => {
   return (
     <section className="sct-book d-flex col" ref={props.refe}>
-      <h2 className="bk-title title">Book a Session</h2>
-      <p>
-        My current rate is $12 / session.
-        Each session is 50 min long.
-      </p>
-      <a target="_blank" rel="noreferrer"
-        href="https://calendly.com/angelspanishcom/session"
-        className="book-btn d-flex ck">
-        <p>Book a session!</p>
-      </a>
-      <div className="bk-disclaimer">
-        <p>No advance payment is required.</p>
-        <p>Pay after our first session!</p>
+      <div className="book-group-1 d-flex col">
+        <h2 className="bk-title title">Book a Session</h2>
+        <div><p>
+          My current rate is $12 / session.
+        </p>
+          <p>
+            Each session is 50 min long.
+          </p>
+        </div>
+        <a target="_blank" rel="noreferrer"
+          href="https://calendly.com/angelspanishcom/session"
+          className="book-btn d-flex ck">
+          <p>Book a session!</p>
+        </a>
+        <div className="bk-disclaimer">
+          <p>No advance payment is required.</p>
+          <p>Pay after our first session!</p>
+        </div>
       </div>
-      <p className="title reviews-title"> Some Reviews </p>
-      <div className="bk-reviews-ctn">
-        <div data-romw-token="xjlAeJMCE9K7Bhp3dhvoGWH5Bpst9BkjUclVengHgPKUQmvB6h"></div>
+      <div className="book-group-2 d-flex col">
+        <p className="title reviews-title"> Some Reviews </p>
+        <div className="bk-reviews-ctn">
+          <div data-romw-token="xjlAeJMCE9K7Bhp3dhvoGWH5Bpst9BkjUclVengHgPKUQmvB6h"></div>
+        </div>
       </div>
+
+
     </section>
   )
 }
--- src/sections/Sessions.js
+++ src/sections/Sessions.js
@@ -5,16 +5,20 @@ const SessionsSection = (props) => {
   return (
     <section className="sct-sessions d-flex col oh" ref={props.refe}>
       <h2 className="ses-title title">Our Sessions</h2>
-      <p>
-        I believe everyone is different so I offer 1-on-1 video meetings. This way I'm able to customize the sessions and the experience for each student. Our sessions will be unique, fulfilling your needs and finding the best path for you.
-      </p>
-      <div className="ses-carousel-ctn d-flex col"><Carousel /></div>
-      <p>
-        I have experience teaching total beginners as well as advanced students who just want to sharpen their Spanish skills. From guys wanting me to help them with Spanish pick-up lines, to doctors wanting to improve their communication with their patients, to people hiring me only to help them pass Spanish-related college exams.
-      </p>
-      <p>
-        It doesn't matter how much Spanish you know (or if you literally just don't know any), I'm capable of help you achieve your goals.
-      </p>
+      <div className="sct-sessions-ctn d-flex col">
+        <div className="ses-carousel-ctn d-flex col"><Carousel /></div>
+        <div className="ses-text d-flex col">
+          <p>
+            I believe everyone is different so I offer 1-on-1 video meetings. This way I'm able to customize the sessions and the experience for each student. Our sessions will be unique, fulfilling your needs and finding the best path for you.
+          </p>
+          <p>
+            I have experience teaching total beginners as well as advanced students who just want to sharpen their Spanish skills. From guys wanting me to help them with Spanish pick-up lines, to doctors wanting to improve their communication with their patients, to people hiring me only to help them pass Spanish-related college exams.
+          </p>
+          <p>
+            It doesn't matter how much Spanish you know (or if you literally just don't know any), I'm capable of help you achieve your goals.
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
