15876dcb166bafa88f984a8cd346cdfb730082de
orozcoding <orozcoding@gmail.com>
2022-07-26 20:31:45 -0400

    Update desktop version about sec

--- src/sections/About.js
+++ src/sections/About.js
@@ -5,9 +5,11 @@ const AboutSection = () => {
         <img className="profile-pic" src="./profilepic.png" alt="profile" />
       </div>
       <div className="abt-ctn-2 d-flex col">
-        <h2 className="title abt-title">¡Hola! I'm Angel and I want to be your Spanish tutor.</h2>
-        <p>I'm a Medical Doctor and a certified Spanish teacher from Venezuela.
-          I've been teaching online people all around the globe since 2018.</p>
+        <div className="abt-text-wrapper d-flex col">
+          <h2 className="title abt-title">¡Hola! I'm Angel and I want to be your Spanish tutor.</h2>
+          <p>I'm a Medical Doctor and a certified Spanish teacher from Venezuela.
+            I've been teaching online people all around the globe since 2018.</p>
+        </div>
       </div>
     </section>
   )
