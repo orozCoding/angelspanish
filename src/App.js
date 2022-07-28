d6f3d79cd3bbd9432b8278363afe7c19c0e72501
orozcoding <orozcoding@gmail.com>
2022-07-27 23:27:58 -0400

    Add spring

--- src/App.js
+++ src/App.js
@@ -6,23 +6,27 @@ import ContactSection from './sections/Contact';
 import FooterSection from './sections/Footer';
 import Header from './sections/Header';
 import SessionsSection from './sections/Sessions';
+import { animated, useSpring, easings } from 'react-spring'
 
 function App() {
   const sessionsRef = useRef(null);
   const bookRef = useRef(null);
   const contactRef = useRef(null);
-
-  // const refs = [sessionsRef];
+  const springStyle = useSpring({ to: { opacity: 1, y: 0 }, from: { opacity: 0, y: -100 },
+  config: { duration: 1500,
+    easing: easings.easeInOutQuart } });
 
   return (
-    <div className="App">
-      <Header refes={{sessionsRef, bookRef, contactRef}} />
-      <AboutSection />
-      <SessionsSection refe={sessionsRef} />
-      <BookSection refe={bookRef} />
-      <ContactSection refe={contactRef} />
-      <FooterSection />
-    </div>
+    <animated.div style={springStyle}>
+      <div className="App">
+        <Header refes={{ sessionsRef, bookRef, contactRef }} />
+        <AboutSection />
+        <SessionsSection refe={sessionsRef} />
+        <BookSection refe={bookRef} />
+        <ContactSection refe={contactRef} />
+        <FooterSection />
+      </div>
+    </animated.div>
   );
 }
 
